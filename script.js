101aa354bc2c3dd5192ece4dd58bac843115fcf5
Artem <butan85@gmail.com>
2022-08-13 12:32:11 +0300

    task 2  Hex Colors ( Responsive Design )

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-var index = 0;
+/* var index = 0; */
 
-function changeColors(){
-    var colors = ["red","blue","orange","yellow","green","purple"];
+function changeColor(){
+    var hex_numbers = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
 
-    document.getElementsByTagName("body")[0].
-style.background = colors[index++];
+    var hexcode = '';
 
-if(index > colors.length - 1)
-index = 0;
+    for(var i=0; i < 6; i++ ){
+        var r = Math.random()
+        var random_index = Math.floor(r * hex_numbers.length);
+        /* console.log(r) */
+        hexcode += hex_numbers[random_index];
+        /* console.log(hexcode) */
+    }
+    document.getElementById("hex-code").innerHTML = hexcode;
+    document.body.style.backgroundColor = "#" + hexcode;
+
 }
